3634288ea5d9a1c0a51d87d7aecf3eab11ff755d
Timothy Callow <t.callow@hzdr.de>
2021-05-14 17:09:58 +0200

    Add functions to occupy KS orbitals via fd statistics

--- AvAtom/staticKS.py
+++ AvAtom/staticKS.py
@@ -10,6 +10,7 @@ import numpy as np
 # internal modules
 import config
 import numerov
+import mathtools
 
 
 class Orbitals:
@@ -40,5 +41,52 @@ class Orbitals:
         # solve the KS equations with the bare coulomb potential
         eigfuncs, eigvals = numerov.matrix_solve(v_en, config.xgrid)
 
+        # redefine lmax
+        config.lmax = len(eigvals[0])
+
+        # keep only the real parts
         self.eigfuncs = eigfuncs
         self.eigvals = eigvals
+
+        # initial guess for the chemical potential
+        config.mu = [0.0, 0.0]
+
+    def occupy(self):
+        """
+        Occupies the orbitals according to Fermi Dirac statistics.
+        The chemical potential is calculated to satisfy charge neutrality
+        within the Voronoi sphere
+        """
+
+        # compute the chemical potential using the eigenvalues
+        config.mu = mathtools.chem_pot(self.eigvals)
+        print(config.mu)
+
+        # compute the occupation numbers using the chemical potential
+        self.occnums = self.calc_occnums(config.mu)
+
+        return True
+
+    def calc_occnums(self, mu):
+        """
+        Computes the Fermi-Dirac occupations for the eigenvalues
+        """
+
+        # initialize the occnums to have the same format
+        occnums = self.eigvals
+
+        if config.spinpol == True:
+            for i in range(2):
+                for l in range(config.lmax):
+                    occnums[i][l] = mathtools.fermi_dirac(
+                        self.eigvals[i][l], mu[i], config.beta
+                    )
+
+        else:
+            for l in range(config.lmax):
+                occnums[0][l] = mathtools.fermi_dirac(
+                    self.eigvals[0][l], mu[0], config.beta
+                )
+            occnums[1] = occnums[0]
+
+        return occnums
