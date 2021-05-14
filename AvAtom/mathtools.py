9a51267e72af46f6377b5d4dfbb89f08d103976d
Timothy Callow <t.callow@hzdr.de>
2021-05-14 12:59:13 +0200

    Add FD function and integral function

--- AvAtom/mathtools.py
+++ AvAtom/mathtools.py
@@ -3,10 +3,11 @@ Low-level module containing various mathematical functions
 """
 
 # standard libraries
-from math import sqrt, pi
+from math import sqrt, pi, exp
 
 # external libraries
 import numpy as np
+import scipy.integrate
 
 # internal libraries
 import config
@@ -54,3 +55,42 @@ def int_sphere(fx):
     I_sph = np.trapz(func_int, config.xgrid)
 
     return I_sph
+
+
+def fermi_dirac(eps, mu, beta, n=0):
+    """
+    Computes the Fermi-Dirac function
+    f_fd = (eps)^(n/2) / (1 + exp(beta(eps-mu))
+
+    Inputs:
+    - mu   (float)  : chemical potential
+    - beta (float)  : inverse temperature
+    - eps  (float)  : energy
+    - n    (int)    : power to which energy is raised (opt)
+    Returns:
+    - f_fd (float)  : fermi_dirac occupation
+    """
+
+    # dfn the exponential function
+    fn_exp = exp(beta * (eps - mu))
+
+    # fermi_dirac dist
+    f_fd = (eps) ** (n / 2.0) / (1 + fn_exp)
+
+    return f_fd
+
+
+def fd_int_complete(mu, beta, n):
+    """
+    Computes complete Fermi-Dirac integrals of the form
+    I_(n/2)(mu, beta) = \int_0^inf \de e^(n/2) f_fd(mu, e, beta)
+
+    Inputs:
+    - mu (float)    : chemical potential
+    - beta (float)  : inverse temperature
+    - n (int)       : order of integral I_(n/2)
+    Returns
+    - I_n (float)   : fd integral
+    """
+
+    return integrate.quad(fermi_dirac, 0, np.inf, args=(mu, beta, n))
