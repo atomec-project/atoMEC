b8a3fa9cde9a7ad62c97727403a5905a2337570f
Timothy Callow <t.callow@hzdr.de>
2021-06-07 15:30:19 +0200

    Update to make accurate for AvAtom installation

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
-# -*- coding: utf-8 -*-
-
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name="AvAtom",
+    version="0.1.0",
+    description="KS-DFT average-atom code",
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author="Tim Callow",
+    author_email="t.callow@hzdr.de",
+    url="https://github.com/timcallow/AvAtom",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=["numpy", "scipy", "mendeleev", "tabulate", "pylibxc2"],
+    python_requires=">=3.6",
 )
-
