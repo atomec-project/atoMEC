34287b2dc5ae2c293d50a43a86f4592b09c6a994
Timothy Callow <t.callow@hzdr.de>
2021-07-07 15:17:15 +0200

    add Python object references for functions listed in module preamble

--- atoMEC/mathtools.py
+++ atoMEC/mathtools.py
@@ -5,17 +5,18 @@ Functions
 ---------
 :func:`normalize_orbs`: normalize KS orbitals within defined sphere
 
-int_sphere: integral :math:`4\pi \int \mathrm{d}r r^2 f(r)`
+:func:`int_sphere`: integral :math:`4\pi \int \mathrm{d}r r^2 f(r)`
 
-lapalce: compute the second-order derivative :math:`d^2 y(x) / dx^2`
+:func:`laplace`: compute the second-order derivative :math:`d^2 y(x) / dx^2`
 
-fermi_dirac: compute the Fermi-Dirac occupation function (for given order n)
+:func:`fermi_dirac`: compute the Fermi-Dirac occupation function (for given order n)
 
-fd_int_complete: compute the complete Fermi-Dirac integral (for given order n)
+:func:`fd_int_complete`: compute the complete Fermi-Dirac integral (for given order n)
 
-chem_pot: compute the chemical potential by enforcing charge neutrality
+:func:`chem_pot`: compute the chemical potential by enforcing charge neutrality
 
-f_root_id: make root input function for chem_pot with ideal approx for free electrons
+:func:`f_root_id`: make root input function for chem_pot with ideal approx for 
+free electrons
 """
 
 # standard libraries
@@ -208,7 +209,7 @@ def chem_pot(orbs):
 
     Parameters
     ----------
-    orbs : :obj:`staticKS.Orbitals`
+    orbs : :class:`staticKS.Orbitals`
         the orbitals object
 
     Returns
