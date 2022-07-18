71657a11a036ca4a99ebeb8f2e4eebb46d9557ea
Nathan <nathan.rahat@mail.huji.ac.il>
2022-07-18 16:14:16 +0300

    Added styling...

--- tests/conductivity_test.py
+++ tests/conductivity_test.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-"""
-Test conductivity module.
-
-"""
+"""Test conductivity module."""
 
 from atoMEC import Atom, models, config
 from atoMEC.postprocess import conductivity
@@ -12,18 +9,13 @@ import numpy as np
 
 
 # expected values and tolerance
-#orbitals_expected = 1.1015
-#density_expected = 2.1252
-#IPR_expected = 78.2107
-#epdc_orbs_expected = 8.7733
-#epdc_dens_expected = 3.5307
-N_cc_expected=4.59790383269
-N_tt_expected=4.59790383269
-N_vv_expected=0.0
-N_cv_expected=0.0
-expected_integral_4=1.341640786499
-expected_integral_2=0.447213595499
-expected_sum_rule=0.2940819621
+N_cc_expected = 4.59790383269
+N_tt_expected = 4.59790383269
+N_vv_expected = 0.0
+N_cv_expected = 0.0
+expected_integral_4 = 1.341640786499
+expected_integral_2 = 0.447213595499
+expected_sum_rule = 0.2940819621
 accuracy = 0.001
 
 
@@ -31,9 +23,8 @@ class TestConductivity:
     """
     Test class for conduction module.
 
-    First runs an SCF calculation to generate KS orbitals and density with and
-    without spin polarization. Then uses this input to check the N_shell and epdc
-    properties in the ELF class, and the IPR matrix.
+    Calculates number of electrons vie the cond_tot function,
+    and checks the sum rule for an unfinished SCF calculation.
     """
 
     @pytest.fixture(autouse=True, scope="class")
@@ -58,56 +49,62 @@ class TestConductivity:
             expected,
             atol=accuracy,
         )
-    
 
     @pytest.mark.parametrize(
         "order,expected",
         [
-            (2,expected_integral_2),
-            (4,expected_integral_4),
+            (2, expected_integral_2),
+            (4, expected_integral_4),
         ],
     )
-    def test_integrals(self,order,expected):
-        """ Test the spherical harmonic integrals. """
-        assert np.isclose(self._run_int_calc(order),expected,atol=accuracy)
+    def test_integrals(self, order, expected):
+        """Test the spherical harmonic integrals."""
+        assert np.isclose(self._run_int_calc(order), expected, atol=accuracy)
 
     @pytest.mark.parametrize(
         "SCF_input,expected",
         [
-            (lazy_fixture("SCF_output"),expected_sum_rule),
+            (lazy_fixture("SCF_output"), expected_sum_rule),
         ],
     )
-    
-    def test_sum_rule(self,SCF_input,expected):
-        assert np.isclose(self._run_sum_rule(SCF_input),expected,atol=accuracy)
-    
+    def test_sum_rule(self, SCF_input, expected):
+        """Run the sum_rule func from the conductivity module."""
+        assert np.isclose(self._run_sum_rule(SCF_input), expected, atol=accuracy)
+
     @staticmethod
     def _run_SCF(spinpol):
-        """
-        Run an SCF calculation for a Fluorine atom
+        r"""
+        Run an SCF calculation for a Fluorine atom.
+
+        Parameters
+        ----------
+        spinpol: bool
+            If the SCF calculation is spin polarized.
 
         Returns
         -------
         output : dict of objects
-            the output dictionary containing density, orbitals etc
+            the output dictionary containing density, orbitals etc.
+
         """
         # parallel
         config.numcores = -1
 
         # set up the atom and model
         F_at = Atom("F", 0.4, radius=5.0)
-        model = models.ISModel(F_at, unbound="quantum",bc="dirichlet")
+        model = models.ISModel(F_at, unbound="quantum", bc="dirichlet")
 
         # run the SCF calculation
-        output = model.CalcEnergy(4,4,scf_params={"mixfrac": 0.3, "maxscf": 6},grid_params={"ngrid": 1200})
+        output = model.CalcEnergy(
+            4, 4, scf_params={"mixfrac": 0.3, "maxscf": 6}, grid_params={"ngrid": 1200}
+        )
 
         return output
-    
-    @staticmethod
 
+    @staticmethod
     def _run_cond_tot(input_SCF, which):
         """
-        Compute the number of conducting electrons via the cond_tot function
+        Compute the number of conducting electrons via the cond_tot function.
 
         Parameters
         ----------
@@ -118,32 +115,60 @@ class TestConductivity:
         Returns
         -------
         N : float
-            number of conductiong electrons in the component
-        """
-        cond=conductivity.KuboGreenwood(input_SCF["orbitals"])
+            number of conductiong electrons in the component.
 
-        N=cond.cond_tot(component=which)[1]
+        """
+        cond = conductivity.KuboGreenwood(input_SCF["orbitals"])
+
+        N = cond.cond_tot(component=which)[1]
         return N
 
     @staticmethod
     def _run_int_calc(order):
         """
+        Calculate a spherical harmonic integral.
+
+        Parameters
+        ----------
+        order : int
+            the order of the P integral to be calculated.
+
+        Returns
+        -------
+        integ: float
+            the resulting integral.
+
         """
-        spherical_har=conductivity.SphHamInts()
-        integ=spherical_har.P_int(order,1,2,1)
+        spherical_har = conductivity.SphHamInts()
+        integ = spherical_har.P_int(order, 1, 2, 1)
         return integ
 
     @staticmethod
-    def _run_sum_rule(input_SCF): 
+    def _run_sum_rule(input_SCF):
         """
+        Check the sum rule of an unconveged set of orbitals.
+
+        Parameters
+        ----------
+        input_SCF : dict of objects
+            the SCF input
+
+        Returns
+        -------
+        sum_rule : float
+            the result of trying to apply the sum rule to an
+            arbitrary inconverged orbital.
+
         """
-        cond=conductivity.KuboGreenwood(input_SCF["orbitals"])
-        sum_rule=cond.check_sum_rule(2,2,0)[0]
+        cond = conductivity.KuboGreenwood(input_SCF["orbitals"])
+        sum_rule = cond.check_sum_rule(2, 2, 0)[0]
         return sum_rule
 
+
 if __name__ == "__main__":
-    SCF_out = TestConduction._run_SCF(False)
-    print(TestConduction._run_cond_tot(SCF_out, "cc"))
-    print(TestConduction._run_cond_tot(SCF_out, "tt"))
-    print(TestConduction._run_cond_tot(SCF_out, "cv"))
-    print(TestConduction._run_cond_tot(SCF_out, "vv"))
+    SCF_out = TestConductivity._run_SCF(False)
+    print(TestConductivity._run_cond_tot(SCF_out, "cc"))
+    print(TestConductivity._run_cond_tot(SCF_out, "tt"))
+    print(TestConductivity._run_cond_tot(SCF_out, "cv"))
+    print(TestConductivity._run_cond_tot(SCF_out, "vv"))
+    print(TestConductivity._run_sum_rule(SCF_out))
