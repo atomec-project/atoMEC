e9768e66990d92add02e0bd2cdf75544c6246bc6
Timothy Callow <t.callow@hzdr.de>
2022-10-11 16:25:13 +0200

    tidy up pressure module

--- atoMEC/postprocess/pressure.py
+++ atoMEC/postprocess/pressure.py
@@ -1,22 +1,50 @@
+"""
+The pressure module contains functions to compute the pressure with various approaches.
+
+Functions
+---------
+* :func: `stress_tensor` : Calculate electronic pressure with stress-tensor method.
+* :func: `virial` : Calculate electronic pressure with virial method.
+* :func: `calc_Wd_xc` : Calculate the derivative contribution to virial pressure.
+* :func: `ions_ideal` : Calculate ionic pressure with ideal gas law.
+"""
+
+# external libs
 import numpy as np
-from atoMEC import unitconv, mathtools, xc
+
+# internal libs
+from atoMEC import mathtools, xc
+from atoMEC.check_inputs import InputError
 
 
 def stress_tensor(orbs, pot):
+    r"""Calculate the pressure with the stress tensor approach [9]_.
 
-    r"""Calculate the pressure with the stress tensor approach."""
+    Parameters
+    ----------
+    orbs : staticKS.Orbitals
+        the orbitals object
+    pot : staticKS.Potential
+        the potential object
 
+    Returns
+    -------
+    P : float
+        the electronic pressure
+
+    References
+    ----------
+    .. [9] G. Faussurier and C. Blancard, Pressure in warm and hot dense matter
+       using the average-atom model, Phys. Rev. E 99, 053201 (2019)
+       `DOI:10.1103/PhysRevE.99.053201 <https://doi.org/10.1103/PhysRevE.99.053201>`__.
+    """
+    # retrive the dimensions of the eigenvalues
     nkpts, spindims, lmax, nmax = np.shape(orbs.eigvals)
 
-    # set the xgrid and value of r at sphere edge
+    # set the xgrid
     xgrid = orbs._xgrid
-    rgrid = np.exp(xgrid)
-    rmax = np.exp(xgrid[-1])
 
     # first compute the gradient of the orbitals
-
-    # take the derivative of orb2
-    # compute the gradient of the orbitals
     deriv_orbs = np.gradient(orbs.eigfuncs, xgrid, axis=-1, edge_order=2)
 
     # chain rule to convert from dP_dx to dX_dr
@@ -47,16 +75,44 @@ def stress_tensor(orbs, pot):
     # put everything together
     P = np.einsum("ijkl,ijklm->m", orbs.occnums_w, sum_terms) / 6
 
-    # integrate
-    # P = mathtools.int_sphere(P, xgrid) / mathtools.int_sphere(
-    #     np.ones_like(xgrid), xgrid
-    # )
-
-    return P[-1]  # * np.exp(2 * xgrid)
+    # return the value of P at the sphere edge
+    return P[-1]
 
 
 def virial(atom, model, energy, density):
+    r"""Compute the pressure using the virial theorem (see notes).
 
+    Parameters
+    ----------
+    atom : atoMEC.Atom
+        the Atom object
+    model : models.ISModel
+        the ISModel object
+    energy : staticKS.Energy
+        the Energy object
+    density : staticKS.Density
+        the density object
+
+    Returns
+    -------
+    pressure : float
+        the electronic pressure
+
+    Notes
+    -----
+    The virial pressure is given by the formula [10]_
+    .. math::
+
+        P = \frac{2T + E_\mathrm{en} + E_\mathrm{Ha} + W_\mathrm{xc}}{3V}\ , \\
+        W_\mathrm{xc} = 3 (E_\mathrm{xc} + W^\mathrm{d}_\mathrm{xc}
+
+    References
+    ----------
+    .. [10] P. Legrand and F. Perrot, Virial theorem and pressure calculations in the
+        GGA, J. Phys.: Condens. Matter 13 (2001) 287–301
+        `DOI:10.1088/0953-8984/13/2/306
+        <https://doi.org/10.1088/0953-8984/13/2/306>`__.
+    """
     # compute the sphere volume
     sph_vol = (4.0 * np.pi / 3.0) * atom.radius**3
 
@@ -70,8 +126,6 @@ def virial(atom, model, energy, density):
     # compute E_V = 2*T + U + W_xc
     E_V = 2 * energy.E_kin["tot"] + energy.E_en + energy.E_ha + W_xc
 
-    print(2 * energy.E_kin["tot"], energy.E_en + energy.E_ha, W_xc)
-
     # compute the virial pressure
     pressure = E_V / (3 * sph_vol)
 
@@ -79,27 +133,67 @@ def virial(atom, model, energy, density):
 
 
 def calc_Wd_xc(xc_func_id, density):
+    r"""Compute the 'derivative' component of the xc term in virial formula (see notes).
 
+    Parameters
+    ----------
+    xc_func_id : str
+        the exchange or correlation functional id
+    density : staticKS.Density
+        the density object
+
+    Returns
+    -------
+    Wd_xc : float
+        the derivative component of the xc term in the virial formula.
+
+    Notes
+    -----
+    In the LDA, Wd_xc is given by [10]_
+
+    .. math::
+
+        W^\mathrm{d}_\mathrm{xc} \int \mathrm{d}r v_\mathrm{xc}(r) n(r)
+
+    GGA implementation is still to come.
+    """
+    # retrieve spin and grid dims
     spindims, ngrid = np.shape(density.total)
 
+    # set up xc_func object
     xc_func = xc.set_xc_func(xc_func_id)
 
     if xc_func_id == "None":
         return 0.0
+    # not set up for GGA funcs yet
+    elif xc_func._family == 2:
+        raise InputError.xc_error("Virial method not yet set up for GGAs")
     else:
         v_xc = xc.calc_xc(density.total, density._xgrid, xc_func, "v_xc")
 
-    # multiply by density and integrate over sphere
+    # multiply by density, integrate over sphere and sum over spins
     Wd_xc = 0.0
     for sp in range(spindims):
-
         Wd_xc += mathtools.int_sphere(density.total[sp] * v_xc[sp], density._xgrid)
 
     return Wd_xc
 
 
 def ions_ideal(atom):
+    r"""Compute the the ideal gas pressure for the ions.
 
+    In atomic units, the ideal gas pressure is simply P = T/V.
+
+    Parameters
+    ----------
+    atom : atoMEC.Atom
+        the Atom object
+
+    Returns
+    -------
+    P_ion : float
+        the ionic pressure
+    """
     # compute the volume
     V = (4.0 * np.pi / 3.0) * atom.radius**3
 
