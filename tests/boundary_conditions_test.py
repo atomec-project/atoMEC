ca4d9d63dda0c379b7f9a2df377f2ca3e9650896
Timothy Callow <t.callow@hzdr.de>
2022-10-21 17:09:52 +0200

    Get rid of failing test

--- tests/boundary_conditions_test.py
+++ tests/boundary_conditions_test.py
@@ -12,8 +12,7 @@ import numpy as np
 
 
 # expected values and tolerance
-# dirichlet_expected = -14.080034
-dirichlet_expected = 0
+dirichlet_expected = -14.080034
 neumann_expected = -15.161942
 bands_expected = -14.731577
 accuracy = 1e-3
