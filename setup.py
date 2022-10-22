8951353be418b3fd558b1bf288e5dd164712c346
Daniel Kotik <d.kotik@hzdr.de>
2022-10-22 19:53:55 +0200

    Add optional dependencies for tests

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ with open("LICENSE") as f:
 extras = {
     'dev': ['bump2version'],
     'docs': open('docs/requirements.txt').read().splitlines(),
+    'tests': open('tests/requirements.txt').read().splitlines(),
 }
 
 setup(
