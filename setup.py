d9b6649eaa1f953cd5edeeaf4ad1b9814dab146f
Timothy Callow <timjcallow@gmail.com>
2023-05-09 13:30:41 +0200

    Bump version: 1.2.0 → 1.3.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ extras = {
 
 setup(
     name="atoMEC",
-    version="1.2.0",
+    version="1.3.0",
     description="KS-DFT average-atom code",
     long_description=readme,
     long_description_content_type='text/markdown',
