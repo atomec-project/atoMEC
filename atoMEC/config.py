b178960263128b0c8bdb61d2daaa7eeffe9312e7
Timothy Callow <timjcallow@gmail.com>
2023-09-29 16:15:15 +0200

    Use a better set of defaults

--- atoMEC/config.py
+++ atoMEC/config.py
@@ -7,16 +7,16 @@ mp_g = 1.6726219e-24  # mass of proton in grams
 spinpol = False  # spin-polarized functional
 xfunc_id = "lda_x"  # exchange functional (libxc ref)
 cfunc_id = "lda_c_pw"  # correlation functional (libxc ref)
-bc = "dirichlet"  # boundary condition
-unbound = "ideal"  # treatment for unbound electrons
+bc = "bands"  # boundary condition
+unbound = "quantum"  # treatment for unbound electrons
 v_shift = True  # whether to shift the KS potential vertically
 
 # numerical grid for static calculations
-grid_params = {"ngrid": 1000, "x0": -12, "ngrid_coarse": 300, "s0": 1e-3}
+grid_params = {"ngrid": 500, "x0": -12, "ngrid_coarse": 300, "s0": 1e-3}
 # convergence parameters for static calculations
 conv_params = {
-    "econv": 1.0e-5,
-    "nconv": 1.0e-4,
+    "econv": 1.0e-6,
+    "nconv": 1.0e-5,
     "vconv": 1.0e-4,
     "eigtol": 1.0e-4,
     "bandtol": 1.0e-3,
@@ -24,12 +24,12 @@ conv_params = {
 # scf parameters
 scf_params = {"maxscf": 50, "mixfrac": 0.3}
 # band parameters for massacrier band model
-band_params = {"nkpts": 50, "de_min": 1e-3}
+band_params = {"nkpts": 30, "de_min": 1e-3}
 
 # forced bound energy levels (default none)
 force_bound = []
 
-grid_type = "log"
+grid_type = "sqrt"
 
 # parallelization
 numcores = 0  # defaults to serial
